1b54b031e0f9787c9b1656d2fe2d181a0ae88e9b
Cyberpro123 <rebycorp321@gmail.com>
2025-03-22 12:09:40 -0700

    added settings.py

---
+++ python/settings.py
@@ -0,0 +1,34 @@
+import argparse
+import configparser
+import os.path
+
+
+def toAbsPath(input: str):
+    return os.path.abspath(os.path.expanduser(input))
+
+
+def setup():
+    global parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", default="config.ini")
+
+
+def parse():
+    global args
+    args = parser.parse_args()
+    global settings
+    settings = {}
+    config = configparser.ConfigParser()
+    config.read(args.config)
+    settings["dirRaws"] = toAbsPath(config["raws"]["dir"])
+    settings["dirOutput"] = toAbsPath(config["output"]["dir"])
+    settings["dirOutHtml"] = config["output"]["html"].strip("/")
+    settings["dirWorkskins"] = config["output"]["workskins"].strip("/")
+    settings["dirAO3CSS"] = config["output"]["ao3css"].strip("/")
+    settings["ao3cssMerged"] = config["ao3"].getboolean("cssMerged")
+    settings["dbFileFull"] = toAbsPath(config["database"]["file"])
+    settings["dirLogs"] = toAbsPath(config["logs"]["dir"])
+    settings["logsTimestamp"] = config["logs"].getboolean("timestamp")
+    settings["ao3DoLogin"] = config["ao3"].getboolean("login")
+    settings["ao3UsernameFile"] = toAbsPath(config["ao3"]["usernameFile"])
+    settings["ao3PasswordFile"] = toAbsPath(config["ao3"]["passwordFile"])
