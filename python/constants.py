7f85b51b12a7dfcb198654a973a09b1da1f998b1
Cyberpro123 <rebycorp321@gmail.com>
2025-03-23 10:15:44 -0700

    Added python3 shebangs to all python files, to avoid any possible misinterpertation as bash commands

--- python/constants.py
+++ python/constants.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 version = "Rapid Development"
 workIdMaxDigits = 10  # Maximum number of digits long an AO3 WorkID might be. at the moment you can probably leave this at 8 and it'll be just fine
 loopRetries = 100  # How many times to retry doing an action that sometimes fails (ie downloading a work) before giving up and throwing an exception
