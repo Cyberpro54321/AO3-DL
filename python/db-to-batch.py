26a2744098fc4bc1db1a7015e1ad488baafbc78c
Cyberpro123 <rebycorp321@gmail.com>
2025-03-26 19:36:41 -0700

    added db-to-batch.py, for exporting just the workIDs from your database to a batch file

---
+++ python/db-to-batch.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import datetime
+import os.path
+
+import database
+import getLogger
+import settings
+
+settings.setup()
+settings.parser.add_argument("input", default="db-to-batch.txt", nargs="?")
+settings.parse()
+config = settings.settings
+
+logCore = "db-to-csv"
+if config["logsTimestamp"]:
+    logAppend = datetime.datetime.now().astimezone().replace(microsecond=0).isoformat()
+else:
+    logAppend = "latest"
+logger = getLogger.getLogger(
+    core=logCore,
+    append=logAppend,
+    dirLogs=config["dirLogs"],
+)
+
+ids = database.getWorkIdSet(filename=config["dbFileFull"], logger=logger)
+
+with open(settings.args.input, "w") as out:
+    for i in ids:
+        out.write(f"{i}\n")
