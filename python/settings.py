3b7804802ce8d6545527941a3b14ee58cdf2b0b2
Cyberpro123 <rebycorp321@gmail.com>
2025-03-30 19:43:44 -0700

    added cli flag overrides for dirRaws, dirLogs, and logsTimestamp, added --version, added help text for --config, changed formatting

--- python/settings.py
+++ python/settings.py
@@ -5,6 +5,8 @@ import configparser
 import logging
 import os.path
 
+import constants
+
 
 def toAbsPath(input: str):
     return os.path.abspath(os.path.expanduser(input))
@@ -12,30 +14,108 @@ def toAbsPath(input: str):
 
 def setup():
     global parser
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", default="config.ini")
+    parser = argparse.ArgumentParser(
+        epilog="Options that start with 'Override' have their defaults set in config.ini.",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=constants.version,
+    )
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="config.ini",
+        help="The config file to use (default: %(default)s)",
+        metavar="",
+    )
+    parser.add_argument(
+        "--raws",
+        type=str,
+        help="Override what directory raw HTML files are expected to be in.",
+        metavar="",
+    )
+    parser.add_argument(
+        "--logs",
+        "-l",
+        type=str,
+        help="Override what directory the log file will be written to.",
+        metavar="",
+    )
+    parser.add_argument(
+        "--logs-timestamp",
+        type=str,
+        help="Override whether to use timestamps in log filenames or not.",
+        metavar="",
+        choices=[
+            "False",
+            "True",
+            "No",
+            "Yes",
+            "false",
+            "true",
+            "no",
+            "yes",
+            "F",
+            "T",
+            "N",
+            "Y",
+            "f",
+            "t",
+            "n",
+            "y",
+        ],
+    )
 
 
 def parse():
     global args
     args = parser.parse_args()
+    print(args.logs_timestamp)
     global settings
     settings = {}
     config = configparser.ConfigParser()
     config.read(args.config)
-    settings["dirRaws"] = toAbsPath(config["raws"]["dir"])
+
+    if args.raws:
+        settings["dirRaws"] = toAbsPath(args.raws)
+    else:
+        settings["dirRaws"] = toAbsPath(config["raws"]["dir"])
+
     settings["dirOutput"] = toAbsPath(config["output"]["dir"])
+
     settings["dirOutHtml"] = config["output"]["html"].strip("/")
+
     settings["dirWorkskins"] = config["output"]["workskins"].strip("/")
+
     settings["dirOutImg"] = config["output"]["images"].strip("/")
+
     settings["dirAO3CSS"] = config["output"]["ao3css"].strip("/")
+
     settings["ao3cssMerged"] = config["ao3"].getboolean("cssMerged")
+
     settings["dbFileFull"] = toAbsPath(config["database"]["file"])
-    settings["dirLogs"] = toAbsPath(config["logs"]["dir"])
-    settings["logsTimestamp"] = config["logs"].getboolean("timestamp")
+
+    if args.logs:
+        settings["dirLogs"] = toAbsPath(args.logs)
+    else:
+        settings["dirLogs"] = toAbsPath(config["logs"]["dir"])
+
+    if args.logs_timestamp is None:
+        settings["logsTimestamp"] = config["logs"].getboolean("timestamp")
+    else:
+        if str(args.logs_timestamp).lower()[:1] in ("y", "t"):
+            settings["logsTimestamp"] = True
+        else:
+            settings["logsTimestamp"] = False
+
     settings["ao3DoLogin"] = config["ao3"].getboolean("login")
+
     settings["ao3UsernameFile"] = toAbsPath(config["ao3"]["usernameFile"])
+
     settings["ao3PasswordFile"] = toAbsPath(config["ao3"]["passwordFile"])
+
     if isinstance(config["logs"]["level"], str):
         for i in (
             ("debug", logging.DEBUG),
@@ -48,3 +128,7 @@ def parse():
                 settings["logsLevel"] = i[1]
     elif isinstance(config["logs"]["level"], int):
         settings["logsLevel"] - config["logs"]["level"]
+
+    for i in ("dirRaws", "dirLogs"):
+        if not os.path.exists(config[i]):
+            raise Exception(f"Specified directory doesn't exist: {settings[i]}")
