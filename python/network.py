634dbc93b03d4627e741574dd5cd400f6a414843
Cyberpro123 <rebycorp321@gmail.com>
2025-04-12 23:56:37 -0700

    expanded discord CDN detection to also work on imgur

--- python/network.py
+++ python/network.py
@@ -78,13 +78,17 @@ def downloadFile(
             urllib.request.urlretrieve(url, f"{dir}/{fileName}")
         except (urllib.error.HTTPError, urllib.error.URLError) as ex:
             random.seed()
-            if (
-                type(ex).__name__ == "HTTPError"
-                and parseResult.netloc[:18].lower() == "cdn.discordapp.com"
+            for i in (
+                ("cdn.discordapp.com", "Discord CDN"),
+                ("i.imgur.com", "Imgur"),
             ):
-                logger.error("Discord CDN Image Hosting Detected")
-                loopNo += 100
-                continue
+                if (
+                    type(ex).__name__ == "HTTPError"
+                    and parseResult.netloc[: len(i[0])].lower() == i[0].lower()
+                ):
+                    logger.error(f"{i[1]} Image Hosting Detected")
+                    loopNo += 100
+                    continue
             if type(ex).__name__ == "HTTPError":
                 pauseLengthRange = (35, 85)
             else:
