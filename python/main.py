9c3bf2e9af187636e358d356625579a962bef472
Cyberpro123 <rebycorp321@gmail.com>
2025-04-12 20:20:15 -0700

    moved main.py to use network.login()

--- python/main.py
+++ python/main.py
@@ -101,18 +101,7 @@ if __name__ == "__main__":
         level=config["logsLevel"],
     )
 
-    if config["ao3DoLogin"]:
-        with open(config["ao3UsernameFile"]) as file:
-            ao3Username = file.readline().strip()
-        with open(config["ao3PasswordFile"]) as file:
-            ao3Password = file.readline().strip()
-        session = network.getSessionObj(
-            username=ao3Username, password=ao3Password, logger=logger
-        )
-        del ao3Username
-        del ao3Password
-    else:
-        session = None
+    session = network.login(config=config, logger=logger)
     work = network.getWorkObjFromId(
         id=batch.parseBatchLine(
             line=settings.args.input,
