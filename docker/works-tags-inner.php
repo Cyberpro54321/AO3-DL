b870ee44a35021c8c413c28dfe08c58c2400069f
Cyberpro123 <rebycorp321@gmail.com>
2025-05-01 17:28:00 -0700

    moved contents of while() into rowToHtmlTags(), switched to switch()

--- docker/works-tags-inner.php
+++ docker/works-tags-inner.php
@@ -1,11 +1,32 @@
 <?php
 
+define("TAG_CATEGORIES", array(
+  array("Fandom", "tagFan"),
+  array("Ship", "tagShips"),
+  array("Character", "tagChar"),
+  array("Freeform", "tagFreeform")
+));
+
 function getOption($input, $option){
   if (($input & pow(2, $option)) == pow(2, $option)) {
     return 1;
   } else {
     return 0;
   }
+}
+function intToArray($input) {
+  $array = [];
+  for ($i=7; $i >= 0; $i--) { 
+    $j = pow(2, $i);
+    if (($input & $j) == $j) {
+      $k = 1;
+      $input -= $j;
+    } else {
+      $k = 0;
+    }
+    $array[$i] = $k;
+  }
+  return $array;
 }
 function rowToHtmlSimple($row) {
   global $dirHTML;
@@ -18,6 +39,149 @@ function rowToHtmlSimple($row) {
   }
   $dateDL = date("Y/m/d", $row['dateLastDownloaded']);
   return "<tr><td>$id</td>$titleLink<td>$chp1/$chp2</td><td>$dateDL</td></tr>";
+}
+function rowToHtmlTags($row) {
+  global $dirHTML, $dbConnect;
+  $id = str_pad($row['ID'], 8, '0', STR_PAD_LEFT);
+  $titleLink = "<a href=../$dirHTML/$id.html>".$row['title']."</a>";
+  $chp1 = $row['chaptersCount'];
+  $chp2 = $row['chaptersExpected'];
+  if ($chp2 == 0) {
+    $chp2 = "?";
+    $reqTagCmp = "<td class='tagIncomplete'>N</td>";
+  } else {
+    if ($chp2 == $chp1) {
+      $reqTagCmp = "<td class='tagComplete'>Y</td>";
+    } else {
+      $reqTagCmp = "<td class='tagIncomplete'>N</td>";
+    }
+  }
+  $dateDL = date("Y/m/d", $row['dateLastDownloaded']);
+  $dateEd = date("Y/m/d", $row['dateLastEdited']);
+  $dateUp = date("Y/m/d", $row['dateLastUpdated']);
+  $q2 = $dbConnect->query("SELECT * FROM tags WHERE ID=$id");
+  $r2 = $q2->fetchArray(SQLITE3_ASSOC);
+  switch ($r2['rating']) {
+    case 1:
+      $reqTagRat = "<td class='ratingN'> </td>";
+      break;
+    case 2:
+      $reqTagRat = "<td class='ratingG'>G</td>";
+      break;
+    case 3:
+      $reqTagRat = "<td class='ratingT'>T</td>";
+      break;
+    case 4:
+      $reqTagRat = "<td class='ratingM'>M</td>";
+      break;
+    case 5:
+      $reqTagRat = "<td class='ratingE'>E</td>";
+      break;
+    default:
+      $reqTagRat = "";
+  }
+  switch ($r2['category']) {
+    case 0:
+      $reqTagCat = "<td class='catN'> </td>";
+      break;
+    case 1:
+      $reqTagCat = "<td class='catFF'>♀</td>";
+      break;
+    case 2:
+      $reqTagCat = "<td class='catFM'>/</td>";
+      break;
+    case 4:
+      $reqTagCat = "<td class='catG'>o</td>";
+      break;
+    case 8:
+      $reqTagCat = "<td class='catMM'>♂</td>";
+      break;
+    case 32:
+      $reqTagCat = "<td class='catO'>?</td>";
+      break;
+    default:
+      $reqTagCat = "<td class='catMu'>!</td>";
+  }
+  $rtw = 0;
+  $warnArray = [];
+  foreach (intToArray($r2['warnings']) as $key => $value) {
+    if ($value == 1) {
+      switch ($key) {
+        case 0:
+          $warnArray[] = "No Archive Warnings Apply";
+          $rtw = max(1, $rtw);
+          break;
+        case 1:
+          $warnArray[] = "Graphic Depictions Of Violence";
+          $rtw = max(2, $rtw);
+          break;
+        case 2:
+          $warnArray[] = "Major Character Death";
+          $rtw = max(2, $rtw);
+          break;
+        case 3:
+          $warnArray[] = "Rape/Non-Con";
+          $rtw = max(2, $rtw);
+          break;
+        case 4:
+          $warnArray[] = "Underage";
+          $rtw = max(2, $rtw);
+          break;
+        case 5:
+          $warnArray[] = "Creator Chose Not To Use Archive Warnings";
+          $rtw = max(3, $rtw);
+      }
+    }
+  }
+  $tagsMain = "";
+  $tagsFan = "";
+  foreach ($warnArray as $value) {
+    $tagsMain .= "<li class='tagWarn'><a>$value</a></li>";
+  }
+  switch ($rtw) {
+    case 3:
+      $reqTagWrn = "<td class='warning6'>?</td>";
+      break;
+    case 2:
+      $reqTagWrn = "<td class='warningX'>!</td>";
+      break;
+    case 1:
+      $reqTagWrn = "<td class='warning1'> </td>";
+      break;
+  }
+  $authorStr = $r2['authorStr'];
+  $summaryStr = $r2['summary'];
+  foreach (TAG_CATEGORIES as $value) {
+    for ($i=1; $i <= 75; $i++) { 
+      $j = $r2["tag$value[0]$i"];
+      if (strlen($j) != 0) {
+        if ($value == TAG_CATEGORIES[0]) {
+          $tagsFan .= "<li class='$value[1]'><a>$j</a></li>";
+        } else {
+          $tagsMain .= "<li class='$value[1]'><a>$j</a></li>";
+        }
+      }
+    }
+  }
+  return "<li>
+    <div class='header'>
+    <table class='requiredTags'>
+    <tr>$reqTagRat$reqTagCat</tr><tr>$reqTagWrn$reqTagCmp</tr>
+    </table>
+    <div>
+    <h4>$titleLink ($id) by $authorStr</h4>
+    <h5><ul class='tags fandoms'>$tagsFan</ul></h5>
+    </div>
+    <div>
+    <p class='timestamp'>Date Updated: $dateUp</p>
+    <p class='timestamp'>Date Edited: $dateEd</p>
+    <p class='timestamp'>Date Downloaded: $dateDL</p>
+    </div>
+    </div>
+    <ul class='tags mainTags'>$tagsMain</ul>
+    <blockquote>$summaryStr</blockquote>
+    <div class='stats'>Chapters: $chp1/$chp2</div>
+    </li>";
 }
 
 # 0 = simple
@@ -77,140 +241,14 @@ $dbConnect = new SQLite3($dbFileRelative, SQLITE3_OPEN_CREATE | SQLITE3_OPEN_REA
 $query = $dbConnect->query("SELECT * FROM works");
 
 while ($row = $query->fetchArray(SQLITE3_ASSOC)) {
-  $chaptNumStr = "";
-  $reqTagRat = "";
-  $reqTagCat = "";
-  $tagsFan = "";
-  $tagsMain = "";
-  foreach ($row as $key => $value) {
-    if ($key == "ID") {
-      $id = str_pad($value, 8, '0', STR_PAD_LEFT);;
-      $q2 = $dbConnect->query("SELECT * FROM tags WHERE ID=$value");
-      $r2 = $q2->fetchArray(SQLITE3_ASSOC);
-    } elseif ($key == "title") {
-      $link = '<a href=../'.$dirHTML.'/'.$id.'.html>'.$value.'</a>';
-    } elseif ($key == "chaptersCount") {
-      $chaptNumStr = $chaptNumStr.$value."/";
-      $chaptCount = $value;
-    } elseif ($key == "chaptersExpected") {
-      if ($value == 0) {
-        $chaptNumStr = $chaptNumStr."?";
-        $reqTagCmp = "<td class='tagIncomplete'>N</td>";
-      } else {
-        $chaptNumStr = $chaptNumStr.$value;
-        if ($value == $chaptCount) {
-          $reqTagCmp = "<td class='tagComplete'>Y</td>";
-        } else {
-          $reqTagCmp = "<td class='tagIncomplete'>N</td>";
-        }
-      }
-    } elseif ($key == "dateLastDownloaded") {
-      $dateDL = date("Y/m/d", $value);
-    } elseif ($key == "dateLastEdited") {
-      $dateEd = date("Y/m/d", $value);
-    } elseif ($key == "dateLastUpdated") {
-      $dateUp = date("Y/m/d", $value);
-    }
+  switch ($mode) {
+    case 0:
+      echo rowToHtmlSimple($row);
+      break;
+    case 1:
+      echo rowToHtmlTags($row);
+      break;
   }
-  foreach ($r2 as $key => $value) {
-    if ($key == "rating") {
-      if ($value == 1) {
-        $reqTagRat = "<td class='ratingN'> </td>";
-      } elseif ($value == 2) {
-        $reqTagRat = "<td class='ratingG'>G</td>";
-      } elseif ($value == 3) {
-        $reqTagRat = "<td class='ratingT'>T</td>";
-      } elseif ($value == 4) {
-        $reqTagRat = "<td class='ratingM'>M</td>";
-      } elseif ($value == 5) {
-        $reqTagRat = "<td class='ratingE'>E</td>";
-      }
-    } elseif ($key == "category") {
-      if ($value == 0) {
-        $reqTagCat = "<td class='catN'> </td>";
-      } elseif ($value == 1) {
-        $reqTagCat = "<td class='catFF'>♀</td>";
-      } elseif ($value == 2) {
-        $reqTagCat = "<td class='catFM'>/</td>";
-      } elseif ($value == 4) {
-        $reqTagCat = "<td class='catG'>o</td>";
-      } elseif ($value == 8) {
-        $reqTagCat = "<td class='catMM'>♂</td>";
-      } elseif ($value == 32) {
-        $reqTagCat = "<td class='catO'>?</td>";
-      } else {
-        $reqTagCat = "<td class='catMu'>!</td>";
-      }
-    } elseif ($key == "warnings") {
-      $rtw = 0;
-      $warnStr = "";
-      if (getOption($value, 5) == 1) {
-        $warnStr = "Creator Chose Not To Use Archive Warnings";
-        $rtw = max(3, $rtw);
-      } elseif (getOption($value, 4) == 1) {
-        $warnStr = "Underage";
-        $rtw = max(2, $rtw);
-      } elseif (getOption($value, 3) == 1) {
-        $warnStr = "Rape/Non-Con";
-        $rtw = max(2, $rtw);
-      } elseif (getOption($value, 2) == 1) {
-        $warnStr = "Major Character Death";
-        $rtw = max(2, $rtw);
-      } elseif (getOption($value, 1) == 1) {
-        $warnStr = "Graphic Depictions Of Violence";
-        $rtw = max(2, $rtw);
-      } elseif (getOption($value, 0) == 1) {
-        $warnStr = "No Archive Warnings Apply";
-        $rtw = max(1, $rtw);
-      }
-      $tagsMain .= "<li class='tagWarn'><a>$warnStr</a></li>";
-      if ($rtw == 3) {
-        $reqTagWrn = "<td class='warning6'>?</td>";
-      } elseif ($rtw == 2) {
-        $reqTagWrn = "<td class='warningX'>!</td>";
-      } elseif ($rtw == 1) {
-        $reqTagWrn = "<td class='warning1'> </td>";
-      }
-    } elseif ($key == "authorStr") {
-      $authorStr = $value;
-    } elseif ($key == "summary") {
-      $summaryStr = $value;
-    } elseif (substr($key, 0, 5) == "tagFa") {
-      if (strlen($value) != 0) {
-        $tagsFan .= "<li class='tagFan'><a>$value</a></li>";
-      }
-    } elseif (substr($key, 0, 4) == "tagS") {
-      if (strlen($value) != 0) {
-        $tagsMain .= "<li class='tagShips'><a>$value</a></li>";
-      }
-    } elseif (substr($key, 0, 4) == "tagC") {
-      if (strlen($value) != 0) {
-        $tagsMain .= "<li class='tagChar'><a>$value</a></li>";
-      }
-    } elseif (substr($key, 0, 5) == "tagFr") {
-      if (strlen($value) != 0) {
-        $tagsMain .= "<li class='tagFreeform'><a>$value</a></li>";
-      }
-    }
-  }
-  echo '<li>';
-  echo '<div class="header">';
-  echo '<table class="requiredTags"><tr>'.$reqTagRat.$reqTagCat.'</tr><tr>'.$reqTagWrn.$reqTagCmp.'</tr></table>';
-  echo '<div>';
-  echo "<h4>$link ($id) by $authorStr</h4>";
-  echo "<h5><ul class='tags fandoms'>$tagsFan</ul></h5>";
-  echo '</div>';
-  echo '<div>';
-  echo '<p class="timestamp">Date Updated: '.$dateUp.'</p>';
-  echo '<p class="timestamp">Date Edited: '.$dateEd.'</p>';
-  echo '<p class="timestamp">Date Downloaded: '.$dateDL.'</p>';
-  echo '</div>';
-  echo '</div>';
-  echo "<ul class='tags mainTags'>$tagsMain</ul>";
-  echo "<blockquote>$summaryStr</blockquote>";
-  echo "<div class='stats'>Chapters: $chaptNumStr</div>";
-  echo '</li>';
-  // print_r($r2);
 }
 
 ?>
