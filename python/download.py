2d61e9a5d4b465ee7cfca66198272b189283656d
Cyberpro123 <rebycorp321@gmail.com>
2025-06-18 16:40:05 -0700

    first draft, untested

--- python/download.py
+++ python/download.py
@@ -1,9 +1,13 @@
+import concurrent.futures
+import datetime
 import os.path
 import pickle
 import random
 import time
+import traceback
 
 import AO3
+import bs4
 
 import constants
 import init
@@ -178,3 +182,133 @@ def login(
     else:
         session = None
     return session
+
+
+def getWorkskin(
+    work: AO3.Work,
+    logger: init.logging.Logger,
+    version: str = constants.version,
+) -> str:
+    workskin = ""
+    try:
+        workskin = work.workskin
+    except AttributeError:
+        allStyles = work._soup.find_all("style", {"type": "text/css"})
+        if len(allStyles) != 0:
+            text = str(allStyles[-1].decode_contents())
+            if text.find("#workskin") != -1:
+                workskin = text
+    if workskin:
+        logger.info(
+            f"Got [{len(workskin)}] characters of workskin from work [{work.id}]"
+        )
+        workskin += f"/*AO3-DL Version: {version}*/"
+        workskin += f"/*AO3-DL Date Saved: {datetime.date.today().isoformat()}*/"
+    else:
+        logger.info(f"Could not find a workskin in work [{work.id}]")
+    return workskin
+
+
+if __name__ == "__main__":
+    ################################################################
+    # Stage 1: Initialization
+    ################################################################
+    init.init(json="r")
+    workIDs = set(())
+    inRaw = init.parseInfile(init.args.infile, init.errLogger)
+    for line in inRaw:
+        try:
+            workIDs.add(int(line))
+        except ValueError:
+            init.errLogger.error(f"Could not convert input [{line}] to int")
+    init.logger(f"Got [{len(workIDs)}] workIDs")
+    ################################################################
+    # Stage 2: Get All Work Objects
+    ################################################################
+    session = login(
+        config=init.config,
+        logger=init.logger,
+        errLogger=init.errLogger,
+    )
+    futures = {}
+    with concurrent.futures.ThreadPoolExecutor(
+        max_workers=10, thread_name_prefix=constants.threadNameBulk
+    ) as pool1:
+        for workID in workIDs:
+            futures[workID] = pool1.submit(
+                getWorkObj,
+                workID=workID,
+                logger=init.logger,
+                errLogger=init.errLogger,
+                ao3Session=session,
+                tryAnon=(not init.config["ao3DoLoginAlways"]),
+            )
+    workObjs = []
+    for workID in futures:
+        try:
+            result = futures[workID].result()
+        except Exception as ex:
+            init.errLogger.error(
+                f"Work [{workID}] raised [{type(ex).__name__}] while getting AO3.Work object: [{ex.args}]"
+            )
+            traceback.print_exception(ex)
+        else:
+            workObjs.append(result)
+    init.logger(f"Got [{len(workObjs)}] AO3.Work objects before blacklist filtering")
+    ################################################################
+    # Stage 3: Blacklist / Whitelist Filtering
+    ################################################################
+
+    workObjsFiltered = workObjs
+    del workObjs
+    init.logger(
+        f"Got [{len(workObjsFiltered)}] AO3.Work objects after blacklist filtering"
+    )
+
+    ################################################################
+    # Stage 4: Save .css and .html files
+    ################################################################
+    workskinsFound = 0
+    for work in workObjsFiltered:
+        workskin = getWorkskin(work=work, logger=init.logger)
+        if workskin:
+            workskinsFound += 1
+            with open(
+                os.path.join(init.config["dirRaws"], f"{work.id:0>8}.css"), "w"
+            ) as file:
+                file.write(workskin)
+    init.logger(f"Wrote [{workskinsFound}] workskin files")
+    futures2 = {}
+    raws = {}
+    with concurrent.futures.ThreadPoolExecutor(
+        max_workers=10, thread_name_prefix=constants.threadNameBulk
+    ) as pool2:
+        for work in workObjsFiltered:
+            futures2[work.id] = pool2.submit(work.download, filetype="HTML")
+    for workID in futures2:
+        try:
+            result = futures2[workID].result()
+        except Exception as ex:
+            init.errLogger.error(
+                f"Work [{workID}] raised [{type(ex).__name__}] while downloading raw: [{ex.args}]"
+            )
+            traceback.print_exception(ex)
+        else:
+            raws[workID] = result
+    for workID in raws:
+        soup = bs4.BeautifulSoup(raws[workID], "lxml")
+        soup.append(bs4.Comment(f"{constants.commentVersion}{constants.version}"))
+        soup.append(
+            bs4.Comment(
+                f"{constants.commentTimestampEdited}{workObjsFiltered[workID].date_edited.isoformat()}"
+            )
+        )
+        soup.append(
+            bs4.Comment(
+                f"{constants.commentTimestampDownloaded}{datetime.datetime.now().isoformat()}"
+            )
+        )
+        with open(
+            os.path.join(init.config["dirOut"], f"{workID:0>8}.html"), "w"
+        ) as file:
+            file.write(soup.prettify(formatter="html5"))
